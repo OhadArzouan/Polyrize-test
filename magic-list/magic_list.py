8087f09b29daa626cfacf4d403640fdb0fa474a2
Ohad Arzouan <ohad.arzouan@gmail.com>
2021-02-15 07:46:46 +0200

    removre .idea

--- magic-list/magic_list.py
+++ magic-list/magic_list.py
@@ -4,18 +4,18 @@ from dataclasses import dataclass
 class MagicList(object):
     def __init__(self, cls_type=None):
         self.cls_type = cls_type
-        if cls_type:
-            self.cls_instance = self.cls_type()
-        self.custom_values = [self.cls_instance] if cls_type else [None]
+        self.custom_values = [self.cls_type()] if self.cls_type else [None]
 
     def __setitem__(self, index, value):
         if index < len(self.custom_values) - 1 or index > len(self.custom_values):
             self.custom_values[index] = value
         else:
             self.custom_values[index] = value
-            self.custom_values.append(self.__init_subclass__self.cls_type)
+            self.custom_values.append(self.cls_type())
 
     def __getitem__(self, index):
+        if index == len(self.custom_values):
+            self.custom_values.append(self.cls_type())
         return self.custom_values[index]
 
 
@@ -25,13 +25,16 @@ class Person:
     weight: int = 5
 
 
-# a = MagicList(cls_type=Person)
-# a[0].age = 5
-# print(a)
-# # a = MagicList()
-# # a[0] = 5
-# # print(a[0])
-# # a[1] = 10
-# # print(a[0],a[1])
-# # a[3] = 10
-# # print(a[0],a[1])
+a = MagicList(cls_type=Person)
+a[0].age = 5
+print(a)
+a[1].age = 7
+print(a[0],a[1])
+
+# a = MagicList()
+# a[0] = 5
+# print(a[0])
+# a[1] = 10
+# print(a[0],a[1])
+# a[3] = 10
+# print(a[0],a[1])
